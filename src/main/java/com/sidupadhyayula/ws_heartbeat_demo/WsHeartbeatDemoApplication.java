05b7af025d66fbfa9b90d91acc564a6db203fcff
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2026-02-08 22:39:29 -0500

    Initial commit

---
+++ src/main/java/com/sidupadhyayula/ws_heartbeat_demo/WsHeartbeatDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sidupadhyayula.ws_heartbeat_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsHeartbeatDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsHeartbeatDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sidupadhyayula/ws_heartbeat_demo/WsHeartbeatDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sidupadhyayula.ws_heartbeat_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsHeartbeatDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
