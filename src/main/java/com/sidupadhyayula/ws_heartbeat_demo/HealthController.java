e566e6e58f111d77b10b6430ddaa2370a24a3361
Sai Siddharth Upadhyayula <usaisiddarth@gmail.com>
2026-02-08 22:50:41 -0500

    Add websocket dependencies and health endpoint

---
+++ src/main/java/com/sidupadhyayula/ws_heartbeat_demo/HealthController.java
@@ -0,0 +1,12 @@
+package com.sidupadhyayula.ws_heartbeat_demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+	@GetMapping("/health")
+	public String health() {
+		return "OK";
+	}
+}
